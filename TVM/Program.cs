14bc4269be763b236425e7624ebc605a9efb1862
ramotk02 <omartaky.ot@gmail.com>
2024-11-27 23:36:58 +0100

    up

--- TVM/Program.cs
+++ TVM/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Magic
+{
+    class Program
+    {
+        static void Main(string[] ags)
+        {
+            Console.Write("1.  Wert: ");
+            string inputline = Console.ReadLine();
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. Wert: ");
+            inputline = Console.ReadLine();
+            int value2 = Convert.ToInt32(inputline);
+
+
+
+
+
+            Console.WriteLine("Nach der Magie!");
+            Console.WriteLine("1. wert: "+ value1);
+            Console.WriteLine("2. Wert: "+ value2);
+
+        }
+    }
+}
