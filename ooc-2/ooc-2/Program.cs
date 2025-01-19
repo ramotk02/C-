2a5066979cfb3e0e491005c635d58520def9d5fe
ramotk02 <omartaky.ot@gmail.com>
2025-01-19 17:09:30 +0100

    up

---
+++ ooc-2/ooc-2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("hello world");
+
+while (true)
+{
+    {
+
+    }
+    Console.WriteLine("What's ur name ?");
+    String a = Console.ReadLine();
+
+    Console.WriteLine("How old are you?");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Hello " + a);
+
+    if (b <= 18)
+    {
+        Console.WriteLine("You're mineur");
+    }
+    else
+    {
+        Console.WriteLine("You're old");
+    }
+}
+
