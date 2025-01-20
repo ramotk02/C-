efc7d47dacf921eb1fb92fbae1a845c7686c0e19
Omar taky <omartaky.ot@gmail.com>
2025-01-20 11:43:28 +0100

    temperature

--- Caffee/Program.cs
+++ Caffee/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static float F2C(int fahr)
+    {
+        return (fahr - 32) * 5 / 9f;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter temperature in Fahrenheit: ");
+        int fahr = int.Parse(Console.ReadLine());
+        float celsius = F2C(fahr);
+        Console.WriteLine($"The temperature in Celsius is: {celsius}°C");
+    }
+}
--- Caffee/obj/Debug/net8.0/Caffee.AssemblyInfo.cs
+++ Caffee/obj/Debug/net8.0/Caffee.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Caffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bca0204a9d31955b6465cd819b07e372dbff59f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490b0680e1d87e6d108505c13bba64d53a411b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Caffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Caffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
