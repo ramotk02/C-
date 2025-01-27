87d5ed2d6804c85566290a42526eefa8406245ed
RamoTk <omartaky.ot@gmail.com>
2025-01-27 12:39:18 +0100

    new project

---
+++ Rabatt/Rabatt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass {
+    
+    public static int nachRabatt(int x, int y)
+    {
+        int result = x - (x * y / 100);
+        return result;
+    }
+
+    public static void Main (string[] args) {
+        Console.WriteLine("Geben Sie ein Preis:");
+        int price = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Wie viel Rabatt haben Sie?");
+        int rabatt = Convert.ToInt32(Console.ReadLine());
+        
+        int nachRabattV = nachRabatt(price, rabatt);
+        Console.WriteLine("Nach Rabatt: " + nachRabattV);
+    }
+}
