a101cd47d965b519d1e431183c0863c9969a7041
RamoTk <omartaky.ot@gmail.com>
2025-05-16 21:30:39 +0200

    up

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        String[] Bigarray = { "B123", "C234", "A345", "C15", "B117", "G3003", "C235", "B179" };
+
+        foreach (string array in Bigarray)
+        {
+
+            Console.WriteLine(array);
+        }
+    }
+
+}
