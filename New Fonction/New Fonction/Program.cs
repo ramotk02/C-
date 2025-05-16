be8e90f60b5ab9d0ccbc793922a4d6497e49f073
RamoTk <omartaky.ot@gmail.com>
2025-05-16 21:20:22 +0200

    up

---
+++ New Fonction/New Fonction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    public static int Hoch(int x, int y)
+    {
+        return (int)Math.Pow(x, y);
+        
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.Write("Geben Sie eine Zahl ein: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Geben Sie den Exponenten ein: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int result = Hoch(a, b);
+        Console.WriteLine($"{a} hoch {b} ist {result}");
+    }
+}
